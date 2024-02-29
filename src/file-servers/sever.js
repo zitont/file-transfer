fe62c249faffdbe4e87df2e50c5b46b018e96a2e
zitont <1456492197@qq.com>
2024-03-01 03:09:04 +0800

    添加时间戳验证上传文件是否过期功能

--- src/file-servers/sever.js
+++ src/file-servers/sever.js
@@ -5,25 +5,89 @@ const path = require('path');
 const app = express();
 const fs = require('fs');
 app.use(cors());
-// 自定义存储方式，保留原始文件名
-// 修改文件存储逻辑，获取文件名并生成时间戳
+// 生成文件存储逻辑，包括设置自定义过期时间并计算过期时间戳
 const storage = multer.diskStorage({
   destination: 'src/file-servers/uploads/',
   filename: function (req, file, cb) {
     const timestamp = Date.now(); // 生成时间戳
-    const newFilename = timestamp + '_' + file.originalname; // 使用时间戳来命名文件
+    let expirationTime = 2 * 60 * 1000; // 默认过期时间为5分钟
+
+    if (req.body.expirationHours) {
+      expirationTime = req.body.expirationHours * 3 * 60 * 60 * 1000; // 使用小时作为过期时间
+    } else if (req.body.expirationMinutes) {
+      expirationTime = req.body.expirationMinutes * 5 * 60 * 1000; // 使用分钟作为过期时间
+    } else if (req.body.expirationDays) {
+      expirationTime = req.body.expirationDays * 24 * 60 * 60 * 1000; // 使用天作为过期时间
+    }
+
+    const expirationTimestamp = timestamp + expirationTime; // 计算过期时间戳
+    const newFilename = expirationTimestamp + '_' + file.originalname; // 使用过期时间戳来命名文件
     const fileInfo = {
       filename: file.originalname, // 文件名
-      timestamp: timestamp // 时间戳
+      timestamp: timestamp, // 上传时间戳
+      expirationTimestamp: expirationTimestamp // 过期时间戳
     };
-    fs.writeFileSync('src/file-servers/fileInfo.json', JSON.stringify(fileInfo)); // 将文件名和时间戳存储到 JSON 文件中
+
+    let fileInfoContent = [];
+    const fileInfoPath = 'src/file-servers/fileInfo.json';
+    if (fs.existsSync(fileInfoPath)) {
+      const fileData = fs.readFileSync(fileInfoPath, 'utf-8');
+      if (fileData.trim() !== '') {
+        fileInfoContent = JSON.parse(fileData);
+      }
+    }
+    fileInfoContent.push(fileInfo);
+    fs.writeFileSync(fileInfoPath, JSON.stringify(fileInfoContent)); // 将文件信息数组存储到 JSON 文件中
     cb(null, newFilename);
   }
 });
   const upload = multer({ storage: storage });
   
+  // 定时事件，处理过期文件
+  function processExpiredFiles() {
+    const fileInfoPath = 'src/file-servers/fileInfo.json';
+    let fileInfoContent = [];
+  
+    if (fs.existsSync(fileInfoPath)) {
+      const fileData = fs.readFileSync(fileInfoPath, 'utf-8').trim();
+      if (fileData !== '') {
+        fileInfoContent = JSON.parse(fileData);
+        const currentTime = Date.now();
+  
+        // 使用 filter 方法创建一个新的数组，其中包含未过期的文件记录
+        fileInfoContent = fileInfoContent.filter((fileInfo) => {
+          if (currentTime > fileInfo.expirationTimestamp) {
+            const filePath = path.join('src/file-servers/uploads/', fileInfo.expirationTimestamp + '_' + fileInfo.filename);
+            // 检查文件是否存在
+            if (fs.existsSync(filePath)) {
+              // 删除过期文件
+              fs.unlink(filePath, (err) => {
+                if (err) {
+                  console.error('Error deleting file:', err);
+                }
+              });
+            } else {
+              console.error('File does not exist:', filePath);
+            }
+            return false; // 返回 false 表示过期文件被删除
+          }
+          return true; // 返回 true 表示文件未过期
+        });
+  
+        // 更新 JSON 文件
+        fs.writeFileSync(fileInfoPath, JSON.stringify(fileInfoContent));
+      } else {
+        console.log('File is empty. Skipping processExpiredFiles.');
+      }
+    }
+    console.log('文件过期处理成功');
+  }
+// 每隔一段时间执行一次处理过期文件的操作
+// setInterval(processExpiredFiles, 24 * 60 * 60 * 1000); // 每隔24小时执行一次
+setInterval(processExpiredFiles, 30 * 1000); //测试
   app.post('/upload', upload.single('file'), (req, res) => {
-    res.send('文件上传成功');
+    res.status(200).send('文件上传成功');
+    console.log('文件上传成功');
   });
 
 
