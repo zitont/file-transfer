09d1087f396ee456888866a3a9d98b4ad38ea2e3
zitont <1456492197@qq.com>
2024-03-01 01:00:59 +0800

    添加ui

--- src/file-servers/sever.js
+++ src/file-servers/sever.js
@@ -3,27 +3,34 @@ const cors = require('cors');
 const multer  = require('multer');
 const path = require('path');
 const app = express();
+const fs = require('fs');
 app.use(cors());
 // 自定义存储方式，保留原始文件名
+// 修改文件存储逻辑，获取文件名并生成时间戳
 const storage = multer.diskStorage({
-    destination: 'src/file-servers/uploads/',
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    }
-  });
-  
+  destination: 'src/file-servers/uploads/',
+  filename: function (req, file, cb) {
+    const timestamp = Date.now(); // 生成时间戳
+    const newFilename = timestamp + '_' + file.originalname; // 使用时间戳来命名文件
+    const fileInfo = {
+      filename: file.originalname, // 文件名
+      timestamp: timestamp // 时间戳
+    };
+    fs.writeFileSync('src/file-servers/fileInfo.json', JSON.stringify(fileInfo)); // 将文件名和时间戳存储到 JSON 文件中
+    cb(null, newFilename);
+  }
+});
   const upload = multer({ storage: storage });
   
   app.post('/upload', upload.single('file'), (req, res) => {
-    // 处理文件上传逻辑
     res.send('文件上传成功');
-    const fileName = req.file.originalname;
-    console.log(fileName+'文件上传成功');
   });
+
+
   app.get('/download/:fileName', (req, res) => {
     const fileName = req.params.fileName;
     const filePath = path.join(__dirname, '../file-servers/uploads', fileName);
-  
+
     // 使用res.download()方法来提供文件下载
     res.download(filePath, fileName, (err) => {
       if (err) {
--- src/views/home/inde.vue
+++ src/views/home/inde.vue
@@ -1,23 +1,84 @@
 <template>
-    <el-upload
-      class="upload-demo"
-      drag
-      action="http://localhost:3000/upload"
-      multiple
-    >
-      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-      <div class="el-upload__text">
-        Drop file here or <em>click to upload</em>
+
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <el-row :gutter="20" class="el-row-sc">
+    <el-col :span="12" :offset="6">
+      <el-card class="box-card" shadow="hover">
+    <template #header>
+      <div class="card-header">
+        <span>上传文件</span>
       </div>
-      <template #tip>
-        <div class="el-upload__tip">
-          jpg/png files with a size less than 500kb
-        </div>
-      </template>
-    </el-upload>
+    </template>
+    <el-upload
+    ref="uploadRef"
+    class="upload-demo"
+    drag
+    action="http://localhost:3000/upload"
+    multiple
+    :on-success="handleSuccess"
+    :auto-upload="false"
+  >
+    <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+    <div class="el-upload__text">
+      将文件拖放到此处或<em>单击以上传</em>
+    </div>
+    <template #tip>
+      <div class="el-upload__tip">
+      </div>
+    </template>
+    
+  </el-upload>
+    <template #footer>
+      <el-button  type="primary" @click="submitUpload">
+      上传<el-icon class="el-icon--right"><Upload /></el-icon>
+    </el-button>
+    </template>
+  </el-card>
+    </el-col>
+  </el-row>
+    
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+
+
+
   </template>
   
   <script setup lang="ts">
-  import { UploadFilled } from '@element-plus/icons-vue'
+import { ref } from "vue";
+import { UploadFilled } from '@element-plus/icons-vue'
+import type { UploadFile} from "element-plus";
+import type { UploadInstance } from 'element-plus'
+import { ElNotification } from 'element-plus'
+const handleSuccess = (response: any, file: UploadFile, fileList: UploadFile[],name: any) => {
+  ElNotification({
+    title: response,
+    message:file.name,
+    type: 'success',
+  })
+  // 处理上传成功后的逻辑
+  console.log(response, file, fileList);
+}
+const uploadRef = ref<UploadInstance>()
+
+const submitUpload = () => {
+  uploadRef.value!.submit()
+  console.log(uploadRef.value)
+}
   </script>
+  <style>
+  .el-row-sc {
+    margin-top: 100px;
+    margin-bottom: 200px;
+  }
+  .box-card {
+  width: 480px;
+  height: 400px;
+}
+</style>
   
